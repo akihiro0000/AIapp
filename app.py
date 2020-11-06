8e175fa73fa891ca1ab6c1c8bd62b2306311ee04
matsudas11 <matsudas11@nlp-dev-pc2.local>
2020-11-06 11:42:42 +0900

    change port again

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=6000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
